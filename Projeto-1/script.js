800f6a2c8fd7d3b2baaf43f2e896a03a82ba743a
LucaSnts4 <166430474+LucaSnts4@users.noreply.github.com>
2024-07-03 19:38:27 -0300

    Add files via upload

---
+++ Projeto-1/script.js
@@ -0,0 +1,20 @@
+let heroi= "jhin";
+let expHeroi= 9620;
+
+if (expHeroi <=1000){
+  console.log("O Herói de nome " + heroi + " está no nível Ferro")
+} else if (expHeroi >=1001 && expHeroi<=2000){
+  console.log("O Herói de nome " + heroi + " está no nível Bronze")
+} else if (expHeroi >=2001 && expHeroi<=5000){
+  console.log("O Herói de nome " + heroi + " está no nível Prata")
+} else if (expHeroi >=5001 && expHeroi<=7000){
+  console.log("O Herói de nome " + heroi + " está no nível Ouro")
+} else if (expHeroi >=7001 && expHeroi<=8000){
+  console.log("O Herói de nome " + heroi + " está no nível Platina")
+} else if (expHeroi >=8001 && expHeroi<=9000){
+  console.log("O Herói de nome " + heroi + " está no nível Ascendente")
+} else if (expHeroi >=9001 && expHeroi<=10000){
+  console.log("O Herói de nome " + heroi + " está no nível Imortal")
+} else if (expHeroi>=10001){
+  console.log("O Herói de nome " + heroi + " está no nível Radiante")
+}
